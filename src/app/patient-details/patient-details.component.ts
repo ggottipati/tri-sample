faab53c8484ed156c0e89d2ecfb88ef2e145fd33
kvuyyuru <kalyanvuyyuru@gmail.com>
2018-09-04 00:36:27 -0500

    Added patients routing logic to show list of patients

---
+++ src/app/patient-details/patient-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Patient } from "../patient-list/model/patients";
+
+@Component({
+  selector: 'app-patient-details',
+  templateUrl: './patient-details.component.html',
+  styleUrls: ['./patient-details.component.css']
+})
+export class PatientDetailsComponent implements OnInit {
+
+  public patients: Patient[] = [
+    new Patient(1, "Patients 001"),
+    new Patient(2, "Patients 002"),
+    new Patient(3, "Patients 003"),
+    new Patient(4, "Patients 004"),
+    new Patient(5, "Patients 005"),
+    new Patient(6, "Patients 006"),
+    new Patient(7, "Patients 007"),
+    new Patient(8, "Patients 008")
+  ];
+  patients: Patient = this.patients[0];// this will store the current product to display
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/patient-list/model/patients.ts
@@ -0,0 +1,8 @@
+export class Patient {
+patientid: number;
+patientName: string;
+constructor(patientid: number, patientName: string) {
+  this.patientid = patientid;
+  this.patientName = patientName;
+}
+}
